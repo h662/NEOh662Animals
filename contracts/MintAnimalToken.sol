f21aa2aba599c3d3ce0b91c99f85ddda62c0168f
h662 <h662hong@gmail.com>
2022-02-17 09:55:41 +0900

    setForSaleAnimalToken

--- contracts/MintAnimalToken.sol
+++ contracts/MintAnimalToken.sol
@@ -3,8 +3,9 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MintAnimals is ERC721Enumerable {
+contract MintAnimalToken is ERC721Enumerable, Ownable {
     constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {}
 
     mapping(uint => string) tokenURIs;
@@ -13,5 +14,13 @@ contract MintAnimals is ERC721Enumerable {
         return string(
             abi.encodePacked("https://allbaaam.mypinata.cloud/ipfs/", tokenURIs[_tokenId])
         );
+    }
+
+    function mintAnimal(string memory _tokenURI) public onlyOwner {
+        uint tokenId = totalSupply() + 1;
+
+        tokenURIs[tokenId] = _tokenURI;
+        
+        _mint(owner(), tokenId);
     }
 }
